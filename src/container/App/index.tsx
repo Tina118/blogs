5bb42ed5f0df14768c111beee2c6032942a69490
Tina118 <tinashah1998@gmail.com>
2023-01-24 18:46:27 +0530

    added basic routing

---
+++ src/container/App/index.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Login from 'container/Login';
+import Posts from 'container/Posts';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="posts" element={<Posts />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
