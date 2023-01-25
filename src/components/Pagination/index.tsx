f881472f7475e3010e52b40fe2b995e3b36e7403
Tina118 <tinashah1998@gmail.com>
2023-01-25 16:39:28 +0530

    added functionality to sync pagination and card

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 interface PaginationProps {
   numberOfTotalPages: number
+  handlePagination: (page: number) => void
 }
 
 class Pagination extends React.Component<PaginationProps> {
@@ -14,27 +15,44 @@ class Pagination extends React.Component<PaginationProps> {
     this.setState({ pages })
   }
 
-  handleClick = (page: number) => this.setState({ selectedPage: page })
+  handleClick = (page: number) => {
+    this.setState({ selectedPage: page })
+    this.props.handlePagination(page)
+  }
 
-  handlePrev = () => {if(this.state.selectedPage!==1){
-    this.setState({selectedPage:this.state.selectedPage-1})
-  }}
+  handlePrev = () => {
+    if (this.state.selectedPage !== 1) {
+      this.setState({ selectedPage: this.state.selectedPage - 1 })
+      this.props.handlePagination(this.state.selectedPage)
+    }
+  }
 
-  handleNext = () => {if(this.state.selectedPage!==this.props.numberOfTotalPages){
-    this.setState({selectedPage:this.state.selectedPage+1})
-  }}
+  handleNext = () => {
+    if (this.state.selectedPage !== this.props.numberOfTotalPages) {
+      this.setState({ selectedPage: this.state.selectedPage + 1 })
+      this.props.handlePagination(this.state.selectedPage)
+    }
+  }
 
   render() {
     return (
       <div className="d-flex justify-content-center align-items-center">
         <ul className="pagination">
-          <li className={`page-item ${this.state.selectedPage===1 ? 'disabled' : ''}`}>
-            <a className="page-link" onClick={this.handlePrev} href="#">Previous</a>
+          <li
+            className={`page-item ${
+              this.state.selectedPage === 1 ? 'disabled' : ''
+            }`}
+          >
+            <a className="page-link" onClick={this.handlePrev} href="#">
+              Previous
+            </a>
           </li>
           {this.state.pages.map((page: number) => (
             <li className="page-item">
               <a
-                className={`page-link ${this.state.selectedPage===page ? 'active' : ''}`}
+                className={`page-link ${
+                  this.state.selectedPage === page ? 'active' : ''
+                }`}
                 href="#"
                 onClick={() => this.handleClick(page)}
               >
@@ -43,8 +61,16 @@ class Pagination extends React.Component<PaginationProps> {
             </li>
           ))}
 
-          <li className={`page-item ${this.state.selectedPage===this.props.numberOfTotalPages ? 'disabled' : ''}`}>
-            <a className="page-link" href="#" onClick={this.handleNext}>Next</a>
+          <li
+            className={`page-item ${
+              this.state.selectedPage === this.props.numberOfTotalPages
+                ? 'disabled'
+                : ''
+            }`}
+          >
+            <a className="page-link" href="#" onClick={this.handleNext}>
+              Next
+            </a>
           </li>
         </ul>
       </div>
--- src/container/Posts/index.tsx
+++ src/container/Posts/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-import PostCard from 'components/PostCard';
-import Pagination from 'components/Pagination';
+import PostCard from 'components/PostCard'
+import Pagination from 'components/Pagination'
 
-const formatData = (response: object[], columns: number, result: object[]): object[] => {
+interface StateProps {
+  posts: object[]
+  numberofTotalPages: number
+  visiblePosts: object[]
+  currentPage: number
+}
+
+const formatData = (
+  response: object[],
+  columns: number,
+  result: object[],
+): object[] => {
   const temp = [...response]
   if (columns <= 0) return result
   while (temp.length) result.push(temp.splice(0, columns))
@@ -11,13 +22,22 @@ const formatData = (response: object[], columns: number, result: object[]): obje
 }
 
 class Posts extends React.Component {
-  state = { posts: [] }
+  state = {
+    posts: [],
+    numberOfTotalPages: 20,
+    visiblePosts: [],
+    currentPage: 1,
+  }
+
   componentDidMount(): void {
     const fetchData = async (): Promise<void> => {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/comments',
       )
       const jsonResponse = await response.json()
+
+      this.setState({ totalPosts: jsonResponse.length })
+
       const result = formatData(jsonResponse, 3, [])
 
       this.setState({ posts: result })
@@ -26,17 +46,40 @@ class Posts extends React.Component {
     fetchData()
   }
 
+  componentDidUpdate(prevProps: any, prevState: StateProps): void {
+    const postsPerPage = Math.floor(
+      this.state.posts.length / this.state.numberOfTotalPages,
+    )
+    const end = this.state.currentPage * postsPerPage
+
+    const start = end - postsPerPage
+
+    const visiblePosts = this.state.posts.slice(start, end)
+
+    console.log(prevState.visiblePosts)
+
+    if(JSON.stringify(prevState.visiblePosts)!==JSON.stringify(visiblePosts)){this.setState({ visiblePosts })}
+    
+
+    console.log(visiblePosts)
+  }
+
+  handlePagination = (page: number) => this.setState({ currentPage: page })
+
   render() {
     return (
       <>
-        {this.state.posts.map((posts: object[]) => (
+        {this.state.visiblePosts.map((posts: object[]) => (
           <div className="card-group">
             {posts.map(({ id, name, email, body }: any) => (
               <PostCard id={id} name={name} email={email} body={body} />
             ))}
           </div>
         ))}
-        <Pagination numberOfTotalPages={20} />
+        <Pagination
+          numberOfTotalPages={this.state.numberOfTotalPages}
+          handlePagination={this.handlePagination}
+        />
       </>
     )
   }
